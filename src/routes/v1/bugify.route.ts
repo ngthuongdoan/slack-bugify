4045ab99db2a1518d809aa9be0b69c5f8e569722
Eddie <thuong.doanngoc@hellohealthgroup.com>
2023-06-09 23:18:01 +0700

    catchAsync

--- src/routes/v1/bugify.route.ts
+++ src/routes/v1/bugify.route.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import express from 'express';
 import { WebClient, LogLevel } from '@slack/web-api';
 import { PoeClient } from 'poe-node-api';
+import catchAsync from '@/utils/catchAsync';
 // WebClient instantiates a client that can call API methods
 // When using Bolt, you can use either `app.client` or the `client` passed to listeners.
 const client = new WebClient(process.env.SLACK_BOT_TOKEN, {
@@ -20,41 +21,38 @@ async function sendMsg(text: string) {
     response = result;
   });
   return response;
-  // await client.chat.update({
-  //   channel: channel || '',
-  //   ts,
-  //   text: response,
-  // });
 }
 
 const router = express.Router();
 
-// let tempText = '';
-router.route('/').post(async (req, res, next) => {
-  await poeClient.init(true);
-  await poeClient.getNextData();
-  const challenge = req.body?.challenge || '';
-  // const type = req.body?.type;
-  const body = req.body as SlackEventPayload;
-  if (body.type === 'event_callback') {
-    console.log(JSON.stringify(body, null, 2));
-    if (body?.event?.type === 'app_mention') {
-      const message = await sendMsg(body.event.text);
-      const response = await client.chat.postMessage({
-        channel: body?.event?.channel || '',
-        text: message,
+router.route('/').post(
+  catchAsync(async (req, res) => {
+    await poeClient.init(true);
+    await poeClient.getNextData();
+    const challenge = req.body?.challenge || '';
+    // const type = req.body?.type;
+    const body = req.body as SlackEventPayload;
+    if (body.type === 'event_callback') {
+      console.log(JSON.stringify(body, null, 2));
+      if (body?.event?.type === 'app_mention') {
+        const message = await sendMsg(body.event.text);
+        console.log('🚀 -------------------------------------------------------------🚀');
+        console.log('🚀 ~ file: bugify.route.ts:39 ~ catchAsync ~ message:', message);
+        console.log('🚀 -------------------------------------------------------------🚀');
+        await client.chat.postMessage({
+          channel: body?.event?.channel || '',
+          text: message,
+        });
+        return res.status(200).send(); // end Slack 3s timeout
+      }
+      return res.status(200).json({
+        text: 'Hello, world.',
       });
-      // const { ts, channel } = response;
-      return res.send(); // end Slack 3s timeout
-      // await sendMsg(ts, channel, body.event.text);
     }
     return res.status(200).json({
-      text: 'Hello, world.',
+      challenge,
     });
-  }
-  return res.status(200).json({
-    challenge,
-  });
-});
+  })
+);
 
 export default router;
