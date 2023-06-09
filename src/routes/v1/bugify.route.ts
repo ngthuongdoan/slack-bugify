58baa552d2ae8cdec56eab814548d73470840115
Eddie <thuong.doanngoc@hellohealthgroup.com>
2023-06-09 14:59:31 +0700

    test mention

--- src/routes/v1/bugify.route.ts
+++ src/routes/v1/bugify.route.ts
@@ -5,6 +5,13 @@ const router = express.Router();
 
 router.route('/').post((req, res, next) => {
   const challenge = req.body?.challenge || '';
+  const type = req.body?.type;
+  if (type === 'event_callback') {
+    if (req.body?.event?.type === 'app_mention')
+      res.status(200).json({
+        text: 'Hello, world.',
+      });
+  }
   res.status(200).json({
     challenge,
   });
