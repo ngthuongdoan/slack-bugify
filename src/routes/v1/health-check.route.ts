7d7524e158af5f4dbacb2366d7037f70a748523e
Eddie <thuong.doanngoc@hellohealthgroup.com>
2023-06-12 14:42:05 +0700

    direct return

--- src/routes/v1/health-check.route.ts
+++ src/routes/v1/health-check.route.ts
@@ -3,12 +3,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.route('/').get(
-  catchAsync(async (req, res) => {
-    return res.send(200).json({
-      ok: true,
-    });
-  })
-);
+router.route('/').get(async (req, res) => {
+  return res.send(200).json({
+    ok: true,
+  });
+});
 
 export default router;
